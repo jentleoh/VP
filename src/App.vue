69e71dcdddda58a8038adb92ed11ed7b53e9a16d
jentleoh <jentleoh@gmail.com>
2022-02-10 19:40:51 +0900

    수정했다

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">홈</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
