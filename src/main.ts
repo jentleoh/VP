c5505bb58d336aef920df848987e484618da4280
jentleoh <jentleoh@gmail.com>
2022-02-10 19:30:27 +0900

    test

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue'
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
 
 new Vue({
   router,
